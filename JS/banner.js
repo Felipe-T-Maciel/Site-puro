6df9fbb1b897ce802ab3da0da9907e60d3d54d24
`felipe-t-maciel` <`felipe_t_maciel@estudante.sesisenai.org.br`>
2025-04-09 00:05:19 -0300

    finish

--- JS/banner.js
+++ JS/banner.js
@@ -5,14 +5,14 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
-    x[i].style.opacity = "0"; // Set opacity to 0 for all slides
-    x[i].style.display = "none"; // Hide all slides
+    x[i].style.opacity = "0";
+    x[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block"; // Show the current slide
+  x[slideIndex-1].style.display = "block"; 
   setTimeout(() => {
-    x[slideIndex-1].style.opacity = "1"; // Fade in the current slide
-  }, 50); // Small delay to trigger the transition
-  setTimeout(carousel, 2000); // Change slide every 2 seconds
+    x[slideIndex-1].style.opacity = "1";
+  }, 50); 
+  setTimeout(carousel, 2000);
 }
